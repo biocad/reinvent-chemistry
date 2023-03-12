edf2709461660c6da216418d4e1a36f462f99666
TimurGimadiev <timur.gimadiev@gmail.com>
2023-03-12 23:52:34 +0300

    new attempt with 3 submodules, Standartized in Libinvent was changed to reinvent-chemistry

--- reinvent_chemistry/__init__.py
+++ reinvent_chemistry/__init__.py
@@ -4,4 +4,5 @@ from reinvent_chemistry.descriptors import Descriptors
 from reinvent_chemistry.utils import get_indices_of_unique_smiles
 from reinvent_chemistry.tokens import TransformationTokens
 from reinvent_chemistry.molecular_transformations import MolecularTransformations
+from reinvent_chemistry.input_standardization import Standardizer
 
